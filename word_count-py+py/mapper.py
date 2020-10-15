13e339cdc043de26a29c630fc1d24425959ae762
cheng bi <879761492@qq.com>
2020-10-15 14:18:08 +0800

    update

--- word_count-py+py/mapper.py
+++ word_count-py+py/mapper.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+import re 
 for line in sys.stdin:
     line = line.strip()
-    words = line.split()
+    words = re.split(r'[\s\,\;\:]+', line)
     for word in words:
         print('{0}\t{1}'.format(word, 1))
